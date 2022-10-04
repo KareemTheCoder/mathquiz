6df4b9be7d3f1e7a78a499564ac79ab21daed19b
KareemTheCoder <112273882+KareemTheCoder@users.noreply.github.com>
2022-10-04 18:37:25 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is 5 x 5",
+    answer: "25",
+    options: [
+      "30",
+      "29",
+      "25",
+      "35"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is 9 x 8",
+    answer: "72",
+    options: [
+      "72",
+      "81",
+      "104",
+      "64"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is 104 + 385",
+    answer: "489",
+    options: [
+      "462",
+      "489",
+      "496",
+      "398"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is 5 x 9",
+    answer: "45",
+    options: [
+      "51",
+      "37",
+      "49",
+      "45"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is 7 x 9",
+    answer: "63",
+    options: [
+      "56",
+      "63",
+      "78",
+      "67"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
